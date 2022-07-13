1cddefcda889a4ead275bb056c041dd41dd6c805
DeboraAndrade <100317147+DebsAndrade@users.noreply.github.com>
2022-07-13 12:44:34 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+function desafio03() {
+  return (
+    <div>
+      <p>
+        Olá Ster e Leandro, segue meu desafio de React 03, do componente de
+        função.
+      </p>
+    </div>
+  );
+}
+
+export default desafio03;
